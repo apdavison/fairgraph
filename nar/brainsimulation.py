3138608a6e47ba8baf4ee7e021c4679b0174f21f
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2019-01-25 17:01:37 +0100

    More work on "simulation" classes

--- nar/brainsimulation.py
+++ nar/brainsimulation.py
@@ -2,14 +2,14 @@
 brain simulation
 """
 
-from .base import KGObject, cache, KGProxy, build_kg_object
+from .base import KGObject, cache, KGProxy, build_kg_object, Distribution, as_list
 from .commons import BrainRegion, CellType, Species, AbstractionLevel
 from .core import Organization, Person
 import datetime
 
 
-#NAMESPACE = "neuralactivity"
-NAMESPACE = "brainsimulation"
+NAMESPACE = "neuralactivity"
+#NAMESPACE = "brainsimulation"
 
 
 class ModelProject(KGObject):
@@ -38,7 +38,7 @@ class ModelProject(KGObject):
         "dateCreated": "schema:dateCreated",
     }
 
-    def __init__(self, name, owner, authors, description, date_created, private, collab_id, alias=None,
+    def __init__(self, name, owners, authors, description, date_created, private, collab_id, alias=None,
                  organization=None, pla_components=None, brain_region=None, species=None, celltype=None,
                  abstraction_level=None, model_of=None, old_uuid=None, id=None, instance=None):
         self.name = name
@@ -49,8 +49,8 @@ class ModelProject(KGObject):
         self.organization = organization
         self.abstraction_level = abstraction_level
         self.private = private
-        self.authors = authors
-        self.owner = owner
+        self.authors = authors  # rename 'contributors', for consistency with MINDS?
+        self.owners = owners
         self.description = description
         self.collab_id = collab_id
         self.PLA_components = pla_components
@@ -71,7 +71,7 @@ class ModelProject(KGObject):
         D = instance.data
         assert 'nsg:ModelProject' in D["@type"]
         obj = cls(name=D["name"], 
-                  owner=None,
+                  owners=None,
                   authors=[],
                   collab_id=D["collabID"], description=D["description"], private=D["private"],
                   date_created=D["dateCreated"],
@@ -91,9 +91,10 @@ class ModelProject(KGObject):
             obj.authors = build_kg_object(Person, D["author"])
         if "owner" in D:
             if isinstance(D["owner"], str):  # temporary, this shouldn't happen once migration complete
+                raise Exception()
                 obj.owner = D["owner"]
             else:
-                obj.owner = build_kg_object(Person, D["owner"])
+                obj.owners = build_kg_object(Person, D["owner"])
         return obj
 
     def save(self, client, exists_ok=True):
@@ -106,7 +107,8 @@ class ModelProject(KGObject):
             }
         if self.authors:
             if isinstance(self.authors, str):  # temporary, should convert into Person
-                data["author"] = self.authors
+                raise Exception()
+                #data["author"] = self.authors
             else:
                 data["author"] = [
                     {
@@ -114,13 +116,23 @@ class ModelProject(KGObject):
                         "@id": person.id
                     } for person in self.authors
                 ]
-        if self.owner:
-            if self.owner.id is None:
-                self.owner.save(client)
-            data["owner"] = {
-                "@type": self.owner.type,
-                "@id": self.owner.id
-            }
+        if self.owners:
+            owners = as_list(self.owners)
+            for owner in owners:
+                if owner.id is None:
+                    owner.save(client)
+            if len(owners) == 1:
+                data["owner"] = {
+                    "@type": owners[0].type,
+                    "@id": owners[0].id
+                }
+            else:
+                data["owner"] = [
+                    {
+                        "@type": owner.type,
+                        "@id": owner.id
+                    } for owner in owners
+                ]
         data["name"] = self.name
         data["collabID"] = self.collab_id
         data["description"] = self.description
@@ -178,10 +190,18 @@ class ModelProject(KGObject):
 
 class ModelInstance(KGObject):
     """docstring"""
-    path = NAMESPACE + "/simulation/modelinstance/v0.1.2"
+    path = NAMESPACE + "/simulation/modelinstance/v0.1.1"   # 0.1.2 is the latest, but only 0.1.1 in nexus currently
     type = ["prov:Entity", "nsg:ModelInstance"]
     # ScientificModelInstance
-    #   - model, version, description, parameters, source, timestamp, code_format, hash, morphology
+    #   - model -> linked ModelProject using partOf
+    #   - version -> add field to ModelInstance.
+    #   - description -> part of Entity
+    #   - parameters -> linked ModelParameters
+    #   - source -> (e.g. git repository) -> linked ModelScript
+    #   - timestamp -> prov:generatedAtTime
+    #   - code_format -> linked ModelScript
+    #   - hash - general feature, don't put in schema
+    #   - morphology - not needed for all models, use MEModel where we have a morphology
     # modelinstance/v0.1.2
     #   - fields of Entity + modelOf, brainRegion, species
 
@@ -199,7 +219,8 @@ class MEModel(ModelInstance):
     #  - fields of ModelInstance + eModel, morphology, mainModelScript, isPartOf (an MEModelRelease)
     
     def __init__(self, name, brain_region, species, model_of, e_model, 
-                 morphology, main_script, release, id=None, instance=None):
+                 morphology, main_script, release, version, timestamp, 
+                 part_of, id=None, instance=None):
         self.name = name
         self.brain_region = brain_region
         self.species = species
@@ -208,6 +229,9 @@ class MEModel(ModelInstance):
         self.morphology = morphology
         self.main_script = main_script
         self.release = release
+        self.version = version
+        self.timestamp = timestamp
+        self.part_of = part_of
         self.id = id
         self.instance = instance
 
@@ -224,20 +248,71 @@ class MEModel(ModelInstance):
                   e_model=build_kg_object(EModel, D["eModel"]),
                   morphology=build_kg_object(Morphology, D["morphology"]),
                   main_script=build_kg_object(ModelScript, D["mainModelScript"]),
-                  release=None,  # to fix once we define MEModelRelease class
+                  release=D.get("release"),  # to fix once we define MEModelRelease class
+                  version=D.get("version"),
+                  timestamp=D.get("generatedAtTime"),  # todo: convert to datetime object
+                  part_of=build_kg_object(D.get("isPartOf")),
                   id=D["@id"], instance=instance)
         return obj
 
+    def save(self, client, exists_ok=True):
+        """docstring"""
+        if self.instance:
+            data = self.instance.data
+        else:
+            data = {
+                "@context": self.context,
+                "@type": self.type
+            }
+        data["name"] = self.name
+        if self.model_of:
+            data["modelOf"] = {
+                "@type": self.model_of.type,
+                "@id": self.model_of.id
+            }
+        if self.brain_region:
+            data["brainRegion"] = self.brain_region.to_jsonld()
+        if self.species:
+            data["species"] = self.species.to_jsonld()
+        data["eModel"] = {
+            "@id": self.e_model.id,
+            "@type": self.e_model.type
+        }
+        data["morphology"] = {
+            "@id": self.morphology.id,
+            "@type": self.morphology.type
+        }
+        if self.main_script:
+            data["mainModelScript"] = {
+                "@id": self.main_script.id,
+                "@type": self.main_script.type
+            }
+        if self.version:
+            data["version"] = self.version
+        if self.timestamp:
+            data["generatedAtTime"] = self.timestamp  # to update once using datetime
+        if self.part_of:
+            data["isPartOf"] = {    
+                "@id": self.part_of.id,
+                "@type": self.part_of.type
+            }
+        if self.release:
+            data["release"] = self.release
+        self._save(data, client, exists_ok)
 
 class Morphology(KGObject):
     path = NAMESPACE + "/simulation/morphology/v0.1.1"
     type = ["prov:Entity", "nsg:Morphology"]
 
     #name, distribution
-    def __init__(self, name, cell_type=None, distribution=None, id=None, instance=None):
+    def __init__(self, name, cell_type=None, morphology_file=None, distribution=None, id=None, instance=None):
         self.name = name
         self.cell_type = cell_type
         self.distribution = distribution
+        if morphology_file and distribution:
+            raise ValueError("Cannot provide both morphology_file and distribution")
+        if morphology_file:
+            self.distribution = Distribution(location=morphology_file)
         self.id = id
         self.instance = instance
 
@@ -248,35 +323,88 @@ class Morphology(KGObject):
         assert 'nsg:Morphology' in D["@type"]
         obj = cls(name=D["name"], 
                   cell_type=D.get("modelOf", None),
-                  distribution=D.get("distribution", None),
+                  distribution=build_kg_object(Distribution, D.get("distribution")),
                   id=D["@id"], instance=instance)
         return obj
+
+    def save(self, client, exists_ok=True):
+        """docstring"""
+        if self.instance:
+            data = self.instance.data
+        else:
+            data = {
+                "@context": self.context,
+                "@type": self.type
+            }
+        data["name"] = self.name
+        if self.cell_type:
+            data["modelOf"] = self.modelOf.to_jsonld()
+        if isinstance(self.distribution, list):
+            data["distribution"] = [item.to_jsonld() for item in self.distribution]
+        else:
+            data["distribution"] = self.distribution.to_jsonld()
+        self._save(data, client, exists_ok)
 
 
 class ModelScript(KGObject):
     path = NAMESPACE + "/simulation/emodelscript/v0.1.0"
-    type = ["prov:Entity", "nsg:EModelScript"]
+    type = ["prov:Entity", "nsg:EModelScript"]  # generalize to other sub-types of script
+    context =  [  # do we need to use "nexus-int" here if we're connecting to it?
+        "https://nexus.humanbrainproject.org/v0/contexts/neurosciencegraph/core/data/v0.3.1",
+        "https://nexus.humanbrainproject.org/v0/contexts/nexus/core/resource/v0.3.0"
+    ]
 
-    def __init__(self, name, distribution, id=None, instance=None):
+    def __init__(self, name, code_location=None, code_format=None, distribution=None, id=None, instance=None):
         self.name = name
         self.distribution = distribution
+        self.code_format = code_format
         self.id = id
         self.instance = instance
+        if code_location and distribution:
+            raise ValueError("Cannot provide both code_location and distribution")
+        if code_location:
+            self.distribution = Distribution(location=code_location)
+        assert isinstance(self.distribution, Distribution)
+
+    @property
+    def code_location(self):
+        return self.distribution.location
 
     @classmethod
     @cache
     def from_kg_instance(cls, instance, client):
         D = instance.data
-        assert 'nsg:EModelScript' in D["@type"]
+        assert 'nsg:EModelScript' in D["@type"]  # generalise
         obj = cls(name=D["name"], 
-                  distribution=D.get("distribution", None),
+                  distribution=build_kg_object(Distribution, D.get("distribution")),
                   id=D["@id"], instance=instance)
         return obj
+
+    def save(self, client, exists_ok=True):
+        """docstring"""
+        if self.instance:
+            data = self.instance.data
+        else:
+            data = {
+                "@context": self.context,
+                "@type": self.type
+            }
+        data["name"] = self.name
+        if isinstance(self.distribution, list):
+            data["distribution"] = [item.to_jsonld() for item in self.distribution]
+        else:
+            data["distribution"] = self.distribution.to_jsonld()
+        self._save(data, client, exists_ok)
 
 
 class EModel(ModelInstance):
     path = NAMESPACE + "/simulation/emodel/v0.1.1"
     type = ["prov:Entity", "nsg:EModel"]
+    context = [
+        "https://nexus.humanbrainproject.org/v0/contexts/neurosciencegraph/core/data/v0.3.1",
+        "https://nexus.humanbrainproject.org/v0/contexts/nexus/core/resource/v0.3.0"
+    ]
+
 
     # model_script, name, species, subCellularMechanism
     def __init__(self, name, brain_region, species, model_of, 
@@ -301,9 +429,38 @@ class EModel(ModelInstance):
                   brain_region=build_kg_object(BrainRegion, D.get("brainRegion")),
                   species=build_kg_object(Species, D.get("species")),
                   main_script=build_kg_object(ModelScript, D["modelScript"]),
-                  release=None,  # to fix once we define MEModelRelease class
+                  release=D.get("release"),  # to fix once we define MEModelRelease class
                   id=D["@id"], instance=instance)
         return obj
+
+
+    def save(self, client, exists_ok=True):
+        """docstring"""
+        if self.instance:
+            data = self.instance.data
+        else:
+            data = {
+                "@context": self.context,
+                "@type": self.type
+            }
+        data["name"] = self.name
+        if self.model_of:
+            data["modelOf"] = {
+                "@type": self.model_of.type,
+                "@id": self.model_of.id
+            }
+        if self.brain_region:
+            data["brainRegion"] = self.brain_region.to_jsonld()
+        if self.species:
+            data["species"] = self.species.to_jsonld()
+        if self.main_script:
+            data["modelScript"] = {
+                "@id": self.main_script.id,
+                "@type": self.main_script.type
+            }
+        if self.release:
+            data["release"] = self.release
+        self._save(data, client, exists_ok)
 
 
 class ValidationProject(KGObject):  # or ValidationProtocol
