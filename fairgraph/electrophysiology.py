44b7c6fef6b1de24afadbb755ea94f6a031621f8
GMattheisen <gmattheisen@gmail.com>
2019-10-14 12:50:35 +0200

    Name to Names

--- fairgraph/electrophysiology.py
+++ fairgraph/electrophysiology.py
@@ -33,7 +33,7 @@ class Trace(KGObject):
         "providerId": "nsg:providerId",
         "description": "schema:description",
         "channel": "nsg:channel",
-		"channelNames": "nsg:channelNames",
+        "channelName": "nsg:channelName",
         "projectName": "nsg:projectName",
         "dataUnit": "nsg:dataUnit",
         "timeStep": "nsg:timeStep",
