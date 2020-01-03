9eade3337b324b6d468bf6be2ce64760bc49c2cd
GMattheisen <gmattheisen@gmail.com>
2020-01-03 11:14:11 +0100

    update

--- fairgraph/electrophysiology.py
+++ fairgraph/electrophysiology.py
@@ -1232,7 +1232,7 @@ class IntraCellularSharpElectrodeExperiment(PatchClampExperiment):
 class QualifiedMultiTraceGeneration(KGObject):
     namespace = DEFAULT_NAMESPACE
     #_path = "/electrophysiology/multitracegeneration/v0.1.0" # for nexus
-    path = DEFAULT_NAMESPACE + "/electrophysiology/multitracegeneration/v0.2.1"  # for nexus-int
+    _path = "/electrophysiology/multitracegeneration/v0.2.2"
     type = ["prov:Generation", "nsg:MultiTraceGeneration"]
     context = {
         "schema": "http://schema.org/",
