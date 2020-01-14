543e0cd54ab2b0f26d2f20e6f601efe8e2fc6a50
GMattheisen <gmattheisen@gmail.com>
2020-01-14 15:47:45 +0100

    test

--- fairgraph/base.py
+++ fairgraph/base.py
@@ -1020,7 +1020,7 @@ class KGQuery(object):
             if api == "nexus":
                 instances = client.query_nexus(
                     path=cls.path,
-                    filter=self.filter["nexus"],
+                    filter=self.filter,
                     context=self.context,
                     size=size
                 )
