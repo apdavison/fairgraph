a274567ecaaabd0a4152936a74e556a556314b77
GMattheisen <gmattheisen@gmail.com>
2020-01-14 13:27:44 +0100

    test

--- fairgraph/minds.py
+++ fairgraph/minds.py
@@ -327,7 +327,7 @@ class Dataset(MINDSObject):
                 elif name == "method":
                     filter_queries.append({
                         #        collection      / patchedslice / patchclampactivity / person
-                        'path': 'https://schema.hbp.eu/minds/specimen_group / https://schema.hbp.eu/minds/subjects / https://schema.hbp.eu/minds/Sample / https://schema.hbp.eu/minds/methods',
+                        'path': 'minds.SpecimenGroup / minds.Subject / minds.Sample / minds.methods',
                         'op': 'eq',
                         'value': value.id
                     })
