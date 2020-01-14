7b9ccbb6b993fe87cc634748a8be1bc340413706
GMattheisen <gmattheisen@gmail.com>
2020-01-14 11:37:34 +0100

    test

--- fairgraph/client.py
+++ fairgraph/client.py
@@ -99,7 +99,7 @@ class KGClient(object):
         else:
             raise ValueError("'api' must be either 'nexus' or 'query'")
         return [cls.from_kg_instance(instance, self, resolved=resolved)
-                for instance in instances], "routing to KGclient"
+                for instance in instances], print("routing to KGClient")
 
     def count(self, cls, api="query", scope="released"):
         """docstring"""
