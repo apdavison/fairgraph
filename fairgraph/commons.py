a59d9d5a1514d44ea824e4147d27017adc3bdc8d
GMattheisen <gmattheisen@gmail.com>
2020-01-16 14:52:07 +0100

    added strain

--- fairgraph/commons.py
+++ fairgraph/commons.py
@@ -112,6 +112,7 @@ class Strain(OntologyTerm):
         "C57BL/6": "http://www.hbp.FIXME.org/hbp_strain_ontology/12345671",
         "C57BL/6J X SJL": "http://www.hbp.FIXME.org/hbp_strain_ontology/12345672",
         "C57BL/6J": "https://www.jax.org/strain/000664",
+        "C57BL/6J-Tg(Thy1-GCaMP6f)GP5.5Dkim/J":"https://www.jax.org/strain/024276",
         "B6.129-Nlgn3<tm4Sud>/J": "https://www.jax.org/strain/023398",
         #"Sprague-Dawley": "https://rgd.mcw.edu/rgdweb/report/strain/main.html?id=70508",
         "Sprague-Dawley": "https://rgd.mcw.edu/rgdweb/ontology/view.html?acc_id=RS:0000681",
