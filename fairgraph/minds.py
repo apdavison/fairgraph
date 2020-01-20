ba272cc8ed185be51abbd65fe6ef1258223ce949
GMattheisen <gmattheisen@gmail.com>
2020-01-20 10:56:22 +0100

    test

--- fairgraph/minds.py
+++ fairgraph/minds.py
@@ -308,10 +308,10 @@ class Dataset(MINDSObject):
                     "op": "and",
                     "value": filter_queries
                 }
-            filter_query = {"query": filter_query}
+            filter_query = {"nexus": filter_query}
             return KGQuery(cls, filter_query, context).resolve(client, api="nexus", size=size)
         elif api == "query":
-            return super(Dataset, cls).list(client, size, from_index, api,
+            return super(PatchedCell, cls).list(client, size, from_index, api,
                                                 scope, resolved, **filters)
         else:
             raise ValueError("'api' must be either 'nexus' or 'query'")
