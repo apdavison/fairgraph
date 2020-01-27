521d3fa8cb1e7213e5b568860a7cc1c5e7ebfa5a
GMattheisen <gmattheisen@gmail.com>
2020-01-27 13:47:14 +0100

    update

--- fairgraph/base.py
+++ fairgraph/base.py
@@ -549,7 +549,7 @@ class KGObject(with_metaclass(Registry, object)):
                 # not sure about the appropriate scope here
             else:
                 raise ValueError("'api' must be 'nexus', 'query' or 'any'")
-            if response:
+            if response is not None:
                 self.id = response[0].data["@id"]
                 KGObject.save_cache[self.__class__][query_cache_key] = self.id
             return bool(response)
