e9b9e31168f30d6ee0c6adcecc0563f3e03ce9e1
GMattheisen <gmattheisen@gmail.com>
2020-01-27 16:59:50 +0100

    update

--- fairgraph/base.py
+++ fairgraph/base.py
@@ -509,7 +509,7 @@ class KGObject(with_metaclass(Registry, object)):
                 query_parts.append({
                     "path": standard_context[field.path],
                     "op": "eq",
-                    "value": field.serialize(getattr(self, field.name), None)
+                    "value": field.serialize(getattr(self, field.id), None)
                 })
             if len(query_fields) == 1:
                 return query_parts[0]
