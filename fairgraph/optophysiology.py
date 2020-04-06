60a7be28af3f66dc3fdf6ec49da1862f12eb0d80
Glynis Mattheisen <gmattheisen@gmail.com>
2020-04-06 17:14:19 +0200

    two photon imaging path

--- fairgraph/optophysiology.py
+++ fairgraph/optophysiology.py
@@ -37,6 +37,7 @@ from datetime import datetime
 from .base import KGObject, KGProxy, KGQuery, cache, lookup, build_kg_object, Field, Distribution
 from .commons import QuantitativeValue, BrainRegion, Origin, CellType, StimulusType, License, Shape
 from .core import Subject, Person, Protocol
+from .electrophysiology import Slice
 from .minds import Dataset
 from .utility import compact_uri, standard_context, as_list
 #from .electrophysiology import Trace
