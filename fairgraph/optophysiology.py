c2ed9213950970c1cb7fb39b72425b2465c9b464
Glynis Mattheisen <gmattheisen@gmail.com>
2020-04-07 11:24:03 +0200

    change to imports

--- fairgraph/optophysiology.py
+++ fairgraph/optophysiology.py
@@ -415,7 +415,7 @@ class TwoPhotonImaging(KGObject):
         Field("people", Person, "wasAssociatedWith", multiple=True)
     )
 
-    def __init__(self, name, image_sequence=None, cranial_window=None, slice=None, microscope=None, brain_state=None, anesthesia=None, laser=None, excitation_wavelength=None, power_at_objective=None, collection_wavelength=None, imaging_depth=None, distribution=None, start_time=None, end_time=None, protocol=None, people=None, citation=None, code=None, license=None, id=None, instance=None):
+    def __init__(self, name, image_sequence=None, target=None, microscope=None, brain_state=None, anesthesia=None, laser=None, excitation_wavelength=None, power_at_objective=None, collection_wavelength=None, imaging_depth=None, distribution=None, start_time=None, end_time=None, protocol=None, people=None, citation=None, code=None, license=None, id=None, instance=None):
         args = locals()
         args.pop("self")
         KGObject.__init__(self, **args)
