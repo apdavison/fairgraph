9cf39355d56e6dd11c92cffa7e1ce512047c45df
Andrew Davison <andrew.davison@unic.cnrs-gif.fr>
2020-04-28 23:51:57 +0200

    Drop testing for Python 2.7 and 3.5, add testing for 3.8.

--- setup.py
+++ setup.py
@@ -23,10 +23,9 @@ setup(
         'Topic :: Scientific/Engineering',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
     keywords='nar hbp metadata electrophysiology nexus shacl',
     packages=find_packages(),
