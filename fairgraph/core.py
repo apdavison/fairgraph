d96eb63a30cb27c00b5952ae3bf59e0650503e90
Glynis Mattheisen <gmattheisen@gmail.com>
2020-06-12 15:26:00 +0200

    update

--- fairgraph/core.py
+++ fairgraph/core.py
@@ -252,11 +252,13 @@ class Identifier(KGObject):
         "schema": "http://schema.org/",
         "nsg": "https://bbp-nexus.epfl.ch/vocabs/bbp/neurosciencegraph/core/v0.1.0/",
         "prov": "http://www.w3.org/ns/prov#",
-        "identifier": "schema:identifier"
+        "identifier": "schema:identifier",
+        "name": "schema:name"
     }
     fields = (
-        Field("identifier", basestring, "identifier"),
+        Field("identifier", basestring, "identifier")
         )
+    existence_query_fields = ("identifier")
 
     def __init__(self, identifier=None, id=None, instance=None):
         args = locals()
