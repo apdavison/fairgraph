4a04e0ce84156af55b594d3b419d52f8e01ae0a7
Andrew Davison <andrew.davison@cnrs.fr>
2021-07-02 09:22:58 +0200

    allow multiple corresponding authors for live papers

--- fairgraph/livepapers.py
+++ fairgraph/livepapers.py
@@ -138,7 +138,7 @@ class LivePaper(KGObject):
         Field("date_modified", datetime, "dateModified", required=False, multiple=False),
         Field("version", str, "version", multiple=False),
         Field("original_authors", Person, "author", required=True, multiple=True),
-        Field("corresponding_author_index", int, "correspondingAuthorIndex", required=False, multiple=False),
+        Field("corresponding_author_index", int, "correspondingAuthorIndex", required=False, multiple=True),
         Field("custodian", Person, "owner", required=False, multiple=False),
         Field("live_paper_authors", Person, "contributor", required=False, multiple=True),
         Field("collab_id", str, "collabID", required=True, multiple=False),
