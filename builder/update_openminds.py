apdavison/fairgraph

commit d662db94248c136bb9f5bd609390aea792be03cb

Add `get_full_name()`, `get_short_name()`, `get_description()`, `get_authors()` and `get_developers()` to research product version classes, as appropriate, to get inherited metadata from parents if needed

fairgraph/openminds/computation/validation_test_version.py
@@ -159,3 +159,23 @@ class ValidationTestVersion(KGObject, OMValidationTestVersion):
             version_identifier=version_identifier,
             version_innovation=version_innovation,
         )
+
+    def _get_inherited_property(self, property_name, client, release_status="released"):
+        value = getattr(self, property_name)
+        if value:
+            return value
+        else:
+            parent = self.is_version_of.resolve(client, release_status=release_status)
+            return getattr(parent, property_name)
+
+    def get_full_name(self, client, release_status="released"):
+        return self._get_inherited_property("full_name", client, release_status)
+
+    def get_short_name(self, client, release_status="released"):
+        return self._get_inherited_property("short_name", client, release_status)
+
+    def get_description(self, client, release_status="released"):
+        return self._get_inherited_property("description", client, release_status)
+
+    def get_developers(self, client, release_status="released"):
+        return self._get_inherited_property("developers", client, release_status)

fairgraph/openminds/core/products/model_version.py
@@ -199,3 +199,40 @@ class ModelVersion(KGObject, OMModelVersion):
             version_identifier=version_identifier,
             version_innovation=version_innovation,
         )
+
+    def download(self, local_path, client, accept_terms_of_use=False):
+        if accepted_terms_of_use(client, accept_terms_of_use=accept_terms_of_use):
+            repo = self.repository.resolve(client, release_status=self.release_status or None)
+            if repo.iri.value.startswith("https://object.cscs.ch/v1/AUTH") or repo.iri.value.startswith(
+                "https://data-proxy.ebrains.eu/api/v1/public"
+            ):
+                zip_archive_url = f"https://data.kg.ebrains.eu/zip?container={repo.iri.value}"
+            else:
+                raise NotImplementedError("Download not yet implemented for this repository type")
+            if local_path.endswith(".zip"):
+                local_filename = Path(local_path)
+            else:
+                local_filename = Path(local_path) / (zip_archive_url.split("/")[-1] + ".zip")
+            local_filename.parent.mkdir(parents=True, exist_ok=True)
+            local_filename, headers = urlretrieve(zip_archive_url, local_filename)
+            return local_filename, repo.iri.value
+
+    def _get_inherited_property(self, property_name, client, release_status="released"):
+        value = getattr(self, property_name)
+        if value:
+            return value
+        else:
+            parent = self.is_version_of.resolve(client, release_status=release_status)
+            return getattr(parent, property_name)
+
+    def get_full_name(self, client, release_status="released"):
+        return self._get_inherited_property("full_name", client, release_status)
+
+    def get_short_name(self, client, release_status="released"):
+        return self._get_inherited_property("short_name", client, release_status)
+
+    def get_description(self, client, release_status="released"):
+        return self._get_inherited_property("description", client, release_status)
+
+    def get_developers(self, client, release_status="released"):
+        return self._get_inherited_property("developers", client, release_status)

fairgraph/openminds/core/products/software_version.py
@@ -205,3 +205,23 @@ class SoftwareVersion(KGObject, OMSoftwareVersion):
             version_identifier=version_identifier,
             version_innovation=version_innovation,
         )
+
+    def _get_inherited_property(self, property_name, client, release_status="released"):
+        value = getattr(self, property_name)
+        if value:
+            return value
+        else:
+            parent = self.is_version_of.resolve(client, release_status=release_status)
+            return getattr(parent, property_name)
+
+    def get_full_name(self, client, release_status="released"):
+        return self._get_inherited_property("full_name", client, release_status)
+
+    def get_short_name(self, client, release_status="released"):
+        return self._get_inherited_property("short_name", client, release_status)
+
+    def get_description(self, client, release_status="released"):
+        return self._get_inherited_property("description", client, release_status)
+
+    def get_developers(self, client, release_status="released"):
+        return self._get_inherited_property("developers", client, release_status)

fairgraph/openminds/core/products/web_service_version.py
@@ -161,3 +161,23 @@ class WebServiceVersion(KGObject, OMWebServiceVersion):
             version_identifier=version_identifier,
             version_innovation=version_innovation,
         )
+
+    def _get_inherited_property(self, property_name, client, release_status="released"):
+        value = getattr(self, property_name)
+        if value:
+            return value
+        else:
+            parent = self.is_version_of.resolve(client, release_status=release_status)
+            return getattr(parent, property_name)
+
+    def get_full_name(self, client, release_status="released"):
+        return self._get_inherited_property("full_name", client, release_status)
+
+    def get_short_name(self, client, release_status="released"):
+        return self._get_inherited_property("short_name", client, release_status)
+
+    def get_description(self, client, release_status="released"):
+        return self._get_inherited_property("description", client, release_status)
+
+    def get_developers(self, client, release_status="released"):
+        return self._get_inherited_property("developers", client, release_status)

fairgraph/openminds/publications/live_paper_version.py
@@ -156,3 +156,23 @@ class LivePaperVersion(KGObject, OMLivePaperVersion):
             version_identifier=version_identifier,
             version_innovation=version_innovation,
         )
+
+    def _get_inherited_property(self, property_name, client, release_status="released"):
+        value = getattr(self, property_name)
+        if value:
+            return value
+        else:
+            parent = self.is_version_of.resolve(client, release_status=release_status)
+            return getattr(parent, property_name)
+
+    def get_full_name(self, client, release_status="released"):
+        return self._get_inherited_property("full_name", client, release_status)
+
+    def get_short_name(self, client, release_status="released"):
+        return self._get_inherited_property("short_name", client, release_status)
+
+    def get_description(self, client, release_status="released"):
+        return self._get_inherited_property("description", client, release_status)
+
+    def get_authors(self, client, release_status="released"):
+        return self._get_inherited_property("authors", client, release_status)

fairgraph/openminds/sands/atlas/brain_atlas_version.py
@@ -172,3 +172,40 @@ class BrainAtlasVersion(KGObject, OMBrainAtlasVersion):
             version_identifier=version_identifier,
             version_innovation=version_innovation,
         )
+
+    def download(self, local_path, client, accept_terms_of_use=False):
+        if accepted_terms_of_use(client, accept_terms_of_use=accept_terms_of_use):
+            repo = self.repository.resolve(client, release_status=self.release_status or None)
+            if repo.iri.value.startswith("https://object.cscs.ch/v1/AUTH") or repo.iri.value.startswith(
+                "https://data-proxy.ebrains.eu/api/v1/public"
+            ):
+                zip_archive_url = f"https://data.kg.ebrains.eu/zip?container={repo.iri.value}"
+            else:
+                raise NotImplementedError("Download not yet implemented for this repository type")
+            if local_path.endswith(".zip"):
+                local_filename = Path(local_path)
+            else:
+                local_filename = Path(local_path) / (zip_archive_url.split("/")[-1] + ".zip")
+            local_filename.parent.mkdir(parents=True, exist_ok=True)
+            local_filename, headers = urlretrieve(zip_archive_url, local_filename)
+            return local_filename, repo.iri.value
+
+    def _get_inherited_property(self, property_name, client, release_status="released"):
+        value = getattr(self, property_name)
+        if value:
+            return value
+        else:
+            parent = self.is_version_of.resolve(client, release_status=release_status)
+            return getattr(parent, property_name)
+
+    def get_full_name(self, client, release_status="released"):
+        return self._get_inherited_property("full_name", client, release_status)
+
+    def get_short_name(self, client, release_status="released"):
+        return self._get_inherited_property("short_name", client, release_status)
+
+    def get_description(self, client, release_status="released"):
+        return self._get_inherited_property("description", client, release_status)
+
+    def get_authors(self, client, release_status="released"):
+        return self._get_inherited_property("authors", client, release_status)

fairgraph/openminds/sands/atlas/common_coordinate_space_version.py
@@ -202,3 +202,40 @@ class CommonCoordinateSpaceVersion(KGObject, OMCommonCoordinateSpaceVersion):
             version_identifier=version_identifier,
             version_innovation=version_innovation,
         )
+
+    def download(self, local_path, client, accept_terms_of_use=False):
+        if accepted_terms_of_use(client, accept_terms_of_use=accept_terms_of_use):
+            repo = self.repository.resolve(client, release_status=self.release_status or None)
+            if repo.iri.value.startswith("https://object.cscs.ch/v1/AUTH") or repo.iri.value.startswith(
+                "https://data-proxy.ebrains.eu/api/v1/public"
+            ):
+                zip_archive_url = f"https://data.kg.ebrains.eu/zip?container={repo.iri.value}"
+            else:
+                raise NotImplementedError("Download not yet implemented for this repository type")
+            if local_path.endswith(".zip"):
+                local_filename = Path(local_path)
+            else:
+                local_filename = Path(local_path) / (zip_archive_url.split("/")[-1] + ".zip")
+            local_filename.parent.mkdir(parents=True, exist_ok=True)
+            local_filename, headers = urlretrieve(zip_archive_url, local_filename)
+            return local_filename, repo.iri.value
+
+    def _get_inherited_property(self, property_name, client, release_status="released"):
+        value = getattr(self, property_name)
+        if value:
+            return value
+        else:
+            parent = self.is_version_of.resolve(client, release_status=release_status)
+            return getattr(parent, property_name)
+
+    def get_full_name(self, client, release_status="released"):
+        return self._get_inherited_property("full_name", client, release_status)
+
+    def get_short_name(self, client, release_status="released"):
+        return self._get_inherited_property("short_name", client, release_status)
+
+    def get_description(self, client, release_status="released"):
+        return self._get_inherited_property("description", client, release_status)
+
+    def get_authors(self, client, release_status="released"):
+        return self._get_inherited_property("authors", client, release_status)

fairgraph/openminds/computation/workflow_recipe_version.py
@@ -163,3 +163,23 @@ class WorkflowRecipeVersion(KGObject, OMWorkflowRecipeVersion):
             version_identifier=version_identifier,
             version_innovation=version_innovation,
         )
+
+    def _get_inherited_property(self, property_name, client, release_status="released"):
+        value = getattr(self, property_name)
+        if value:
+            return value
+        else:
+            parent = self.is_version_of.resolve(client, release_status=release_status)
+            return getattr(parent, property_name)
+
+    def get_full_name(self, client, release_status="released"):
+        return self._get_inherited_property("full_name", client, release_status)
+
+    def get_short_name(self, client, release_status="released"):
+        return self._get_inherited_property("short_name", client, release_status)
+
+    def get_description(self, client, release_status="released"):
+        return self._get_inherited_property("description", client, release_status)
+
+    def get_developers(self, client, release_status="released"):
+        return self._get_inherited_property("developers", client, release_status)

commit b666000d4cd1976af4c273b287e72bbd53d04d51

Fix for bugs introduced in [d662db94]

builder/update_openminds.py
@@ -545,6 +545,10 @@ def get_controlled_terms_table(type_):
     return ""
 
 
+preamble_for_download = """from urllib.request import urlretrieve
+from pathlib import Path
+from ....utility import accepted_terms_of_use"""
+
 preamble = {
     "File": """import os
 import mimetypes
@@ -558,22 +562,13 @@ from ...controlled_terms.unit_of_measurement import UnitOfMeasurement
 from fairgraph.utility import accepted_terms_of_use, sha1sum
 
 mimetypes.init()""",
-    "DatasetVersion": """from urllib.request import urlretrieve
-from pathlib import Path
-from ....utility import accepted_terms_of_use""",
-    "ModelVersion": """from fairgraph.errors import ResolutionFailure
-from .model import Model""",
-    "ValidationTestVersion": """from fairgraph.errors import ResolutionFailure
-from .validation_test import ValidationTest""",
-    "LivePaperVersion": """from fairgraph.errors import ResolutionFailure
-from .live_paper import LivePaper""",
+    "DatasetVersion": preamble_for_download,
+    "ModelVersion": preamble_for_download,
+    "BrainAtlasVersion": preamble_for_download,
+    "CommonCoordinateSpaceVersion": preamble_for_download,
     "ScholarlyArticle": """from fairgraph.utility import as_list
 from .publication_issue import PublicationIssue
 from .periodical import Periodical""",
-    "SoftwareVersion": """from fairgraph.errors import ResolutionFailure
-from .software import Software""",
-    "WebServiceVersion": """from fairgraph.errors import ResolutionFailure
-from .web_service import WebService""",
 }
 
 

fairgraph/openminds/computation/validation_test_version.py
@@ -8,8 +8,7 @@ from openminds.properties import Property
 from openminds.latest.computation import ValidationTestVersion as OMValidationTestVersion
 from fairgraph import KGObject
 
-from fairgraph.errors import ResolutionFailure
-from .validation_test import ValidationTest
+
 from datetime import date
 from openminds import IRI
 

fairgraph/openminds/core/products/model_version.py
@@ -8,8 +8,9 @@ from openminds.properties import Property
 from openminds.latest.core import ModelVersion as OMModelVersion
 from fairgraph import KGObject
 
-from fairgraph.errors import ResolutionFailure
-from .model import Model
+from urllib.request import urlretrieve
+from pathlib import Path
+from ....utility import accepted_terms_of_use
 from datetime import date
 from openminds import IRI
 

fairgraph/openminds/core/products/software_version.py
@@ -8,8 +8,7 @@ from openminds.properties import Property
 from openminds.latest.core import SoftwareVersion as OMSoftwareVersion
 from fairgraph import KGObject
 
-from fairgraph.errors import ResolutionFailure
-from .software import Software
+
 from datetime import date
 from openminds import IRI
 

fairgraph/openminds/core/products/web_service_version.py
@@ -8,8 +8,7 @@ from openminds.properties import Property
 from openminds.latest.core import WebServiceVersion as OMWebServiceVersion
 from fairgraph import KGObject
 
-from fairgraph.errors import ResolutionFailure
-from .web_service import WebService
+
 from datetime import date
 from openminds import IRI
 

fairgraph/openminds/publications/live_paper_version.py
@@ -8,8 +8,7 @@ from openminds.properties import Property
 from openminds.latest.publications import LivePaperVersion as OMLivePaperVersion
 from fairgraph import KGObject
 
-from fairgraph.errors import ResolutionFailure
-from .live_paper import LivePaper
+
 from datetime import date
 from datetime import datetime
 from openminds import IRI

fairgraph/openminds/sands/atlas/brain_atlas_version.py
@@ -8,7 +8,9 @@ from openminds.properties import Property
 from openminds.latest.sands import BrainAtlasVersion as OMBrainAtlasVersion
 from fairgraph import KGObject
 
-
+from urllib.request import urlretrieve
+from pathlib import Path
+from ....utility import accepted_terms_of_use
 from datetime import date
 from openminds import IRI
 

fairgraph/openminds/sands/atlas/common_coordinate_space_version.py
@@ -8,7 +8,9 @@ from openminds.properties import Property
 from openminds.latest.sands import CommonCoordinateSpaceVersion as OMCommonCoordinateSpaceVersion
 from fairgraph import KGObject
 
-
+from urllib.request import urlretrieve
+from pathlib import Path
+from ....utility import accepted_terms_of_use
 from datetime import date
 from openminds import IRI
 