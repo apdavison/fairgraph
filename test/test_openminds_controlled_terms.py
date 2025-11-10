7cddd957d55c65c90988da1d877268af180731f0
Andrew Davison <andrew.davison@cnrs.fr>
2025-11-10 14:34:44 +0100

    Add test that an openMINDS controlled terms instance with a non-UUID-based @id returns True for `obj.exists()`

--- test/test_openminds_controlled_terms.py
+++ test/test_openminds_controlled_terms.py
@@ -1,6 +1,14 @@
 import fairgraph.openminds.controlled_terms as terms
+from test.utils import mock_client, kg_client, skip_if_no_connection, skip_if_using_production_server
 
 
 def test_initialization():
     for cls in terms.list_kg_classes():
         obj = cls(name="foo")
+
+
+@skip_if_no_connection
+def test_exists(kg_client):
+    obj = terms.AgeCategory.adult
+    assert obj.space == "controlled"
+    assert obj.exists(kg_client)
